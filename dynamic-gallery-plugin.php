0ede5265bb128afaecddaec29c884f2fb4743466
studiograsshopper <studiograsshopper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-06 05:49:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/dynamic-content-gallery-plugin/tags/2.0@72460 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dynamic-gallery-plugin.php
+++ dynamic-gallery-plugin.php
@@ -3,7 +3,8 @@
 Plugin Name: Dynamic Content Gallery
 Plugin URI: http://www.studiograsshopper.ch/wordpress-plugins/dynamic-content-gallery-plugin-v2/
 Version: 2.0 beta
-Author: <a href="http://www.studiograsshopper.ch/">Ade Walker</a>
+Author: Ade Walker
+Author URI: http://www.studiograsshopper.ch
 Description: Creates a dynamic content gallery anywhere within your wordpress theme using <a href="http://smoothgallery.jondesign.net/">Smooth Gallery</a>. Inspired by the Featured Content Gallery developed by Jason Schuller. Set up the plugin options in Settings>Dynamic Content Gallery.
 */
 
