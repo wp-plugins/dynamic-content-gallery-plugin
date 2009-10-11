5816a580ae79e5b5f6927341941c5e60ce9b9298
studiograsshopper <studiograsshopper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-11 22:29:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/dynamic-content-gallery-plugin/trunk@162492 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -4,7 +4,7 @@
 *	Copyright 2008-2009  Ade WALKER  (email : info@studiograsshopper.ch)
 *
 * 	@package	Dynamic Content Gallery
-*	@version	3.0
+*	@version	3.0 RC3
 *
 *	Uninstall file for WP 2.7+
 *	
@@ -12,6 +12,7 @@
 if ( !defined('WP_UNINSTALL_PLUGIN') ) {
     exit();
 }
+// Delete options from database
 delete_option('dfcg_plugin_settings');
 delete_option('dfcg_version');
 ?>
