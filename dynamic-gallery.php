2589afcc682361fa39ae18df6d53ead6d8a78be6
studiograsshopper <studiograsshopper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-10 21:37:16 +0000

    git-svn-id: https://plugins.svn.wordpress.org/dynamic-content-gallery-plugin/trunk@172148 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dynamic-gallery.php
+++ dynamic-gallery.php
@@ -3,35 +3,68 @@
 *	*******************************************************
 *	Copyright 2008-2009  Ade WALKER  (email : info@studiograsshopper.ch)
 *
-* 	@package	Dynamic Content Gallery
-*	@version	3.0 RC3
+* 	@package	dynamic_content_gallery
+*	@version	3.0 RC4
 *
-*	This is the file that displays the gallery, called by dynamic_content_gallery() function.
+*	This is the file that displays the gallery, called by dynamic_content_gallery()
+*	template tag function.
+*
+*	Note: the name of this file is preserved because some users will still be using the
+*	old method of calling the plugin (now replaced with template tag)
+*
 *	3 methods of populating the gallery as per Settings:
 *		- Multi Option
 *		- One Category
 *		- Pages
-*	
+*
 */
 
+/* Prevent direct access to this file */
+if (!defined('ABSPATH')) {
+	exit(__( "Sorry, you are not allowed to access this file directly.", DFCG_DOMAIN ));
+}
 
-/*	Populate method = MULTI-OPTION */
-if($dfcg_options['populate-method'] == 'multi-option' && function_exists('dfcg_multioption_method_gallery') ) {
-	// Output the gallery
-	dfcg_multioption_method_gallery();
 
-/*	Populate method = PAGES */
-} elseif($dfcg_options['populate-method'] == 'pages' && function_exists('dfcg_pages_method_gallery') ) {
-	// Output the gallery
-	dfcg_pages_method_gallery();
+/*	Determine which scripts are being loaded */
+if( $dfcg_options['scripts'] == 'mootools' ) {
 
-/*	Populate method = ONE CATEGORY */
-} elseif($dfcg_options['populate-method'] == 'one-category' && function_exists('dfcg_onecategory_method_gallery') ) {
-	// Output the gallery
-	dfcg_onecategory_method_gallery();
+	/*	Populate method = MULTI-OPTION */
+	if($dfcg_options['populate-method'] == 'multi-option' && function_exists('dfcg_multioption_method_gallery') ) {
+		// Output the gallery
+		dfcg_multioption_method_gallery();
 
-/*	Something has gone horribly wrong! */
+	/*	Populate method = ONE CATEGORY */
+	} elseif($dfcg_options['populate-method'] == 'one-category' && function_exists('dfcg_onecategory_method_gallery') ) {
+		// Output the gallery
+		dfcg_onecategory_method_gallery();
+
+		/*	Populate method = PAGES */
+	} elseif($dfcg_options['populate-method'] == 'pages' && function_exists('dfcg_pages_method_gallery') ) {
+		// Output the gallery
+		dfcg_pages_method_gallery();
+	}
+
+} elseif( $dfcg_options['scripts'] == 'jquery' ) {
+
+	/*	Populate method = MULTI-OPTION */
+	if($dfcg_options['populate-method'] == 'multi-option' && function_exists('dfcg_jq_multioption_method_gallery') ) {
+		// Output the gallery
+		dfcg_jq_multioption_method_gallery();
+
+	/*	Populate method = ONE CATEGORY */
+	} elseif($dfcg_options['populate-method'] == 'one-category' && function_exists('dfcg_jq_onecategory_method_gallery') ) {
+		// Output the gallery
+		dfcg_jq_onecategory_method_gallery();
+
+		/*	Populate method = PAGES */
+	} elseif($dfcg_options['populate-method'] == 'pages' && function_exists('dfcg_jq_pages_method_gallery') ) {
+		// Output the gallery
+		dfcg_jq_pages_method_gallery();
+	}
+
+/*	Something has gone horribly wrong and there's no output! */
 } else {
+
 	$output = '';
 	$output .= $dfcg_errmsgs['public'];
 	$output .= $dfcg_errmsgs['10'] . "\n";
