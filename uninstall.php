0eb5e67a5d991010889ee449b5b41ca642dd0427
studiograsshopper <studiograsshopper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-09 17:05:04 +0000

    Update 09/10/2011
    git-svn-id: https://plugins.svn.wordpress.org/dynamic-content-gallery-plugin/trunk@449131 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Uninstall file as per WP 2.7+
+ *
+ * @author Ade WALKER  (email : info@studiograsshopper.ch)
+ * @copyright Copyright 2008-2011
+ * @package dynamic_content_gallery
+ * @version 4.0
+ *
+ * Removes options from db when plugin is deleted via Dashboard
+ *
+ * @since 3.2
+ */
+
+/* Prevent direct access to this file */
+if( !defined( 'ABSPATH' ) ) {
+	exit( __( 'Sorry, you are not allowed to access this file directly.' ) );
+}
+
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
+}
+
+// Delete options from database
+delete_option( 'dfcg_plugin_settings' );
+delete_option( 'dfcg_version' );
+delete_option( 'dfcg_plugin_postmeta_upgrade' );
+?>
